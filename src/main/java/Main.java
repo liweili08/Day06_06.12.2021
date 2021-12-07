cddcbf16e4826ccf3fd8219b7ed12dd887d29afd
liwei <liweili@qq.com>
2021-12-07 11:41:04 +0100

    Veränderung

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import model.Student;
+
+public class Main {
+    public static void main(String[] args){
+        Student testStudent = new Student("Anna", 21, false);
+        System.out.println(testStudent.toString());
+
+
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,5 @@
 
+import model.Student;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,34 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentTest {
 @Test
     void TestName(){
-    Student testStudent = new Student();
-    testStudent.setName("Anna");
+    Student testStudent = new Student("Anna", 21, false);
+    //testStudent.setName("Anna");
     assertEquals("Anna", testStudent.getName());
 }
     @Test
     void TestAge(){
-        Student testStudent = new Student();
-        testStudent.setAge(21);
-
+        Student testStudent = new Student("Anna", 21, false);
+        //testStudent.setAge(21);
         assertEquals(21, testStudent.getAge());
     }
 //    @Test
 //    void TestMale(){
-//        Student testStudent = new Student();
+//        model.Student testStudent = new model.Student();
 //        testStudent.setMale(false);
 //        assertFalse(testStudent.getMale());
 //    }
 //    @Test
 //    void TestMale(){
-//        Student testStudent = new Student();
+//        model.Student testStudent = new model.Student();
 //        testStudent.setMale(false);
 //        assertEquals(false,testStudent.getMale());
 //    }
     @Test
     void TestMale(){
-        Student testStudent = new Student();
-        testStudent.setMale(false);
+        Student testStudent = new Student("Anna", 21, false);
+        //testStudent.setMale(false);
         assertEquals(false,testStudent.isMale());
+    }
+
+    @Test
+    void TestEquals(){
+        Student testStudent1 = new Student("Anna", 21, false);
+        Student testStudent2 = new Student("Anne", 21, false);
+        //testStudent.setMale(false);
+        assertEquals(false, testStudent1.equals(testStudent2));
     }
 
 }
